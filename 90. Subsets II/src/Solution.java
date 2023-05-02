1b30e42a567b96722ff606d32b1147f53ff7c6eb
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-05-02 18:27:17 +0200

    Add working solution

---
+++ 90. Subsets II/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<>(), nums, 0);
+
+        return result;
+    }
+
+    public void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, int index) {
+        result.add(new ArrayList<>(tempList));
+        for(int i = index; i<nums.length; i++) {
+            if(i > index && nums[i] == nums[i-1]) continue;
+            tempList.add(nums[i]);
+            backtrack(result, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
