da9d18bf3f8bbae49c1b9cd435ff3b8f305621ab
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-05-11 20:22:58 +0200

    Add working solution

---
+++ 234. Palindrome Linked List/src/SolutionStack.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class SolutionStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(4);
+        if(stack.peek() == 0) {
+            stack.isEmpty()
+
+        }
+
+    }
+
+    public int ok() {
+        if(true) {
+            ok();
+        }
+        return 2;
+    }
+}
