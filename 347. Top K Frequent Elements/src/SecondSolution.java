6ff16de6f53bbdc5fd25aa14aef055e2d5b244bd
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-06-05 20:31:12 +0200

    Add second solution

---
+++ 347. Top K Frequent Elements/src/SecondSolution.java
@@ -0,0 +1,20 @@
+class SecondSolution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>(Comparator.comparingInt(map::get).reversed());
+        heap.addAll(map.keySet());
+
+        int[] ans = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = heap.poll();
+        }
+
+        return ans;
+    }
+}
