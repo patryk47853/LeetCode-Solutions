df0a203d19ee15d14f5dbc8f0cd3ebc30c65133f
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-06-23 19:43:08 +0200

    Add working solution

---
+++ 771. Jewels and Stones/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        int ans = 0;
+
+        for(int i=0; i<jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+
+        for(int i=0; i<stones.length(); i++) {
+            if(set.contains(stones.charAt(i))) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
