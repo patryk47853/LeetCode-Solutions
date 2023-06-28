ebef4fce60f3d67319e38edd0a8a79b67896bbcd
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-06-28 09:57:48 +0200

    Add second solution

---
+++ 1528. Shuffle String/src/SecondSolution.java
@@ -0,0 +1,11 @@
+class SecondSolution {
+    public String restoreString(String s, int[] indices) {
+        char[] str = new char[s.length()];
+
+        for(int i=0; i < s.length(); i++) {
+            str[indices[i]] = s.charAt(i);
+        }
+
+        return String.valueOf(str);
+    }
+}
