b73e56a91bf5792d0efb63ddf0d0bcf02d540969
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-07-15 15:07:31 +0200

    Add second solution

---
+++ 1832. Check if the Sentence Is Pangram/src/SecondSolution.java
@@ -0,0 +1,17 @@
+class SecondSolution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 'a'; i <= 'z'; i++) {
+            set.add((char) i);
+        }
+
+        for (int i = 0; i < sentence.length(); i++) {
+            set.remove(sentence.charAt(i));
+            if (set.isEmpty())
+                return true;
+        }
+
+        return false;
+    }
+}
