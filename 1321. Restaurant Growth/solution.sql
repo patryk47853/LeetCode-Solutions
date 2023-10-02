00c11dc21e2cd1c71b355ecd7dc1a93018a992bb
patryk47853 <109095669+patryk47853@users.noreply.github.com>
2023-10-02 20:40:49 +0200

    Add working solution (MySQL)

---
+++ 1321. Restaurant Growth/solution.sql
@@ -0,0 +1,8 @@
+SELECT a.visited_on AS visited_on, SUM(b.daily_sum) AS amount,
+       ROUND(AVG(b.daily_sum), 2) AS average_amount
+FROM
+    (SELECT visited_on, SUM(amount) AS daily_sum FROM Customer GROUP BY visited_on) a,
+    (SELECT visited_on, SUM(amount) AS daily_sum FROM Customer GROUP BY visited_on) b
+WHERE DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
+GROUP BY a.visited_on
+HAVING COUNT(b.visited_on) = 7;
